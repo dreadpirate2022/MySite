3e1708c1febc1abba875c80dd03a768edff860bb
dreadpirate2022 <mirko.cvetanoski@gmail.com>
2022-12-19 09:22:55 +0100

    Commiting whole page

---
+++ blog/views.py
@@ -0,0 +1,108 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm, SearchForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
+
+# Create your views here.
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
+    # Pagination with 3 post per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page
+        posts = paginator.page(paginator.num_pages)
+
+    context = {'posts':posts, 'tag':tag}
+    return render(request, 'blog/post/list.xhtml', context)
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    context = {'post':post, 'comments':comments, 'form':form, 'similar_posts':similar_posts}
+    return render(request, 'blog/post/detail.xhtml', context)
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # Send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recomends you " + f"{post.title}"
+            message = f"Read {post.title} at {post_url}" + f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'mysitedemo2022@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    context = {'post':post ,'form':form, 'sent':sent}
+    return render(request, 'blog/post/share.xhtml', context)
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to database
+        comment.save()
+        
+    context = {'post':post, 'form':form, 'comment':comment}
+    return render(request, 'blog/post/comment.xhtml', context)
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+
+    context = {'form':form, 'query':query, 'results':results}
+    return render(request, 'blog/post/search.xhtml', context)
+
+# Class based views
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.xhtml'
